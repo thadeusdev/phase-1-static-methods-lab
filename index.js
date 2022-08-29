c51e04c4a7b231a39d1e7bcdba88bb03de1dc0f2
thadeus <thadeusnyariki94@gmail.com>
2022-08-29 16:28:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.toLowerCase().replace(/[^A-Za-z0-9-']+/g, "");
+  }
+  static titleize(string){
+    return string.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()); 
+    
+  }
 }
